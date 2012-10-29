3d9f954aaac85b531f8f76a15be6f2326222bb35
no-rk <nork@p-nor.sakura.ne.jp>
2012-10-29 21:32:48 +0900

    はじまり

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DNU::Application.config.secret_token = 'df321496992d14b5ca5e252e3e17bc89b1fb1c298c56a906748098e4acf722b9c5f2518afc06d4df5da68acc080443af9f7064f0cd8e38946859cd8ff72029c3'
