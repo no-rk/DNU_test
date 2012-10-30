3d5f8f81f09296da365e127e21c9ab121408a7ee
no-rk <nork@p-nor.sakura.ne.jp>
2012-10-30 13:59:34 +0900

    ログイン機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
+
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
+  attr_accessor :login
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :id, :email, :password, :password_confirmation, :remember_me, :login
+  # attr_accessible :title, :body
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(id) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+end
