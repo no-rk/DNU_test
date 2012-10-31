23645870fb1f8503fe8f1bd52d90109b91abe390
no-rk <nork@p-nor.sakura.ne.jp>
2012-10-31 09:42:35 +0900

    Railsのバージョンを3.2.3から3.2.8へ

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DNU::Application.config.secret_token = 'df321496992d14b5ca5e252e3e17bc89b1fb1c298c56a906748098e4acf722b9c5f2518afc06d4df5da68acc080443af9f7064f0cd8e38946859cd8ff72029c3'
+DNU::Application.config.secret_token = '09e8a3112f5fd6130d96cb86885b3e4f477bab976147fabc3b0219e3615897faf5ea2085cba1644c2fca5c0f87dec68a6babd78ea71c5cecfd3612f5fef3d4d2'
